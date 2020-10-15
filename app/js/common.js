46cd419b28e9c1d7f92f15768f50362724724c18
VadimVelikanets <52658557+VadimVelikanets@users.noreply.github.com>
2020-10-15 03:08:58 +0300

    Update common.js

--- app/js/common.js
+++ app/js/common.js
@@ -7,14 +7,16 @@ $(function() {
             $(this).find('img').slideToggle(0);
         }
     )
-    //Открытие пунктов меню на мобилом
+    //Открытие подменю на мобильном
     $('.header__menu .btn-arrow').click(
         function(){
+            $(this).parent().parent().siblings().removeClass('active');
             if($(this).parent().parent().hasClass('active')){
                 $(this).parent().parent().removeClass('active')
             } else{
                 $(this).parent().parent().addClass('active')
             }
+            //Скролл до активного пункта
             let activeBlock = $(this);
             $('html').animate({
                 scrollTop: $(activeBlock).offset().top - 20
